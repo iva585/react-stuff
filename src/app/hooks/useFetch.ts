840ee7adcadcecd2d726daed2dd5e7eca5b1078a
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-11 16:54:26 +0100

    feature: add custom hook useFetch

---
+++ src/app/hooks/useFetch.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export default function useFetch(url: string) {
+  const [data, setData] = useState(null);
+  async function fetchData() {
+    const response = await fetch('url');
+    const newData = await response.json();
+    setData(newData);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return data;
+}
