6bd3752564594e10f954ac9372434972fa2045d1
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-13 16:50:17 +0100

    style: make form look pretty

--- src/app/pages/Add/Add.tsx
+++ src/app/pages/Add/Add.tsx
@@ -1,13 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Add(): JSX.Element {
   return (
-    <form>
-      <label>Name</label>
-      <input type="text" />
-      <label>Description</label>
-      <input type="text" />
-      <button>Submit</button>
-    </form>
+    <StyledForm>
+      <StyledLabel>Name:</StyledLabel>
+      <StyledInput type="text" />
+      <StyledLabel>Description:</StyledLabel>
+      <StyledInput type="text" />
+      <StyledSubmit>Submit</StyledSubmit>
+    </StyledForm>
   );
 }
+
+const StyledForm = styled.form`
+  display: grid;
+  grid-row-gap: 5px;
+  margin: 20px;
+  background: linear-gradient(
+    90deg,
+    rgba(2, 32, 103, 0.8589810924369747) 0%,
+    rgba(121, 9, 106, 1) 35%,
+    rgba(0, 157, 255, 1) 100%
+  );
+  padding: 15px;
+  width: 240px;
+  border-radius: 5px;
+  box-shadow: 0px 10px 13px -7px #000000,
+    5px 5px 15px 5px rgba(39, 43, 145, 0.12);
+`;
+
+const StyledLabel = styled.label`
+  color: white;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  font-stretch: expanded;
+  font-size: 1.1em;
+`;
+
+const StyledInput = styled.input`
+  height: 2em;
+  width: 80%;
+  border-radius: 2px;
+`;
+
+const StyledSubmit = styled.button`
+  background: #ee36d5 35%;
+  padding: 5px;
+  border-radius: 2px;
+  text-transform: uppercase;
+  color: white;
+  margin: 2px;
+  width: 40%;
+`;
