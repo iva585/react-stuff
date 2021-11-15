9855fc445e90ebc6e2bb0f4e208f7d844f41e8b3
Iva Pranjic <pranjic.iva213@gmail.com>
2021-11-15 11:03:02 +0100

    refactor: optimize code for delete button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,10 +1,6 @@
 import styled from 'styled-components';
 
-type DeleteButtonProps = { thingId: number };
-
-const DeleteButton = styled.button.attrs((props: DeleteButtonProps) => ({
-  thingId: props.thingId,
-}))`
+const DeleteButton = styled.button`
   background: #ee36d5 35%;
   width: 30px;
   height: 30px;
